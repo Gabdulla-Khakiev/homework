69cf68b7f25d1f3d8f135d1c2297838ab9de3e59
Gabdulla <gabdulla2001@gmail.com>
2024-08-11 05:40:21 +0300

    add processing.py

---
+++ src/processing.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+def filter_by_state(list_of_dict: List, state='EXECUTED') -> List:
+    """Функция возвращает список отсортированных словарей по параметру state"""
+    pass
