9bf457d0897eb4bdc933991ce1c081bd29dedd7a
Gabdulla <gabdulla2001@gmail.com>
2024-08-13 01:30:52 +0300

    small fixes in processing.py

--- src/processing.py
+++ src/processing.py
@@ -10,6 +10,6 @@ def filter_by_state(list_of_dict: List, state='EXECUTED') -> List:
     return filtered_list
 
 
-def sort_by_date(list_of_dict_2: List, reverse=True):
+def sort_by_date(list_of_dict_2: List, reverse=True) -> List:
     """Функция возвращает отсртированный список по дате"""
     return sorted(list_of_dict_2, key=lambda x: x['date'], reverse=reverse)
