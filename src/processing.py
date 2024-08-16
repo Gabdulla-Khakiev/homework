eceff90cdfa27321f39cf92c4f1525ed7f4ff0e6
Gabdulla <gabdulla2001@gmail.com>
2024-08-16 04:44:36 +0300

    add test_filter_by_state, fixes in processing.py, add fixtures for test_processing.py

--- src/processing.py
+++ src/processing.py
@@ -3,13 +3,20 @@ from typing import List
 
 def filter_by_state(list_of_dict: List, state='EXECUTED') -> List:
     """Функция возвращает список отсортированных словарей по параметру state"""
-    filtered_list = []
-    for i in list_of_dict:
-        if i['state'] == state:
-            filtered_list.append(i)
-    return filtered_list
+    if state == '':
+        state = 'EXECUTED'
+    return [item for item in list_of_dict if item['state'] == state]
 
 
 def sort_by_date(list_of_dict_2: List, reverse=True) -> List:
     """Функция возвращает отсртированный список по дате"""
     return sorted(list_of_dict_2, key=lambda x: x['date'], reverse=reverse)
+
+
+if __name__ == '__main__':
+    print(filter_by_state([
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+    ]))
