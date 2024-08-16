9368ac65eb875f9e9d184d01f64b8c6034f0972c
Gabdulla <gabdulla2001@gmail.com>
2024-08-16 06:06:34 +0300

    add test_sort_by_date, fixes in processing.py, add fixtures for test_processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,22 +1,28 @@
 from typing import List
 
 
-def filter_by_state(list_of_dict: List, state='EXECUTED') -> List:
+def filter_by_state(list_of_dict: List, state='EXECUTED'):
     """Функция возвращает список отсортированных словарей по параметру state"""
-    if state == '':
-        state = 'EXECUTED'
-    return [item for item in list_of_dict if item['state'] == state]
+    if 'state' in list_of_dict:
+        if state == '':
+            state = 'EXECUTED'
+        return [item for item in list_of_dict if item['state'] == state]
+    else:
+        return '"state" not found'
 
 
-def sort_by_date(list_of_dict_2: List, reverse=True) -> List:
+def sort_by_date(list_of_dict: List, reverse=True):
     """Функция возвращает отсртированный список по дате"""
-    return sorted(list_of_dict_2, key=lambda x: x['date'], reverse=reverse)
+    if 'date' in list_of_dict:
+        return sorted(list_of_dict, key=lambda x: x['date'], reverse=reverse)
+    else:
+        return '"date" not found'
 
 
 if __name__ == '__main__':
-    print(filter_by_state([
-        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-    ]))
+    print(sort_by_date([
+        {'id': 41428829, 'state': 'EXECUTED'},
+        {'id': 939719570, 'state': 'EXECUTED'},
+        {'id': 594226727, 'state': 'CANCELED'},
+        {'id': 615064591, 'state': 'CANCELED'}
+    ], ))
--- tests/conftest.py
+++ tests/conftest.py
@@ -24,3 +24,24 @@ def list_for_tests():
         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
     ]
+
+
+@pytest.fixture()
+def empty_state_list_for_tests():
+    """Фикстура с отсутсвием параметра 'state' и одинаковыми датами"""
+    return [
+        {'id': 41428829, 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 939719570, 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 594226727, 'date': '2018-06-30T02:08:58.425572'},
+        {'id': 615064591, 'date': '2018-06-30T02:08:58.425572'}
+    ]
+
+
+@pytest.fixture()
+def empty_date_list_for_tests():
+    return [
+        {'id': 41428829, 'state': 'EXECUTED'},
+        {'id': 939719570, 'state': 'EXECUTED'},
+        {'id': 594226727, 'state': 'CANCELED'},
+        {'id': 615064591, 'state': 'CANCELED'}
+    ]
