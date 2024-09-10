ecfff75477bffeb7e55af22404526d420364717a
Gabdulla <gabdulla2001@gmail.com>
2024-09-11 02:45:01 +0300

    small fixes and add files ".log"

--- src/masks.py
+++ src/masks.py
@@ -4,7 +4,7 @@ from typing import Union
 
 logger = logging.getLogger("masks")
 logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler("logs/masks.log", mode="w")
+file_handler = logging.FileHandler("logs/masks.log", mode='w', encoding='utf8')
 file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
@@ -12,6 +12,9 @@ logger.addHandler(file_handler)
 
 def get_mask_card_number(card_number: Union[str]) -> Union[str]:
     """Функция возвращает замаскированный номер карты"""
+
+    logger.info("Запуск работы функции")
+
     tmp_list = []
     counter = 0
     if isinstance(card_number, str):
@@ -27,21 +30,27 @@ def get_mask_card_number(card_number: Union[str]) -> Union[str]:
                 if counter % 4 == 0 and counter < 16:
                     tmp_list.append(" ")
             masked_card_number = "".join(tmp_list)
+            logger.info("Звершение работы функции")
 
             return masked_card_number
         else:
             logger.error(f"Произошла ошибка: Введен не номер карты")
+            logger.info("Звершение работы функции")
             return "Это не номер карты!"
     else:
         logger.error(f"Произошла ошибка: Введен не номер карты")
+        logger.info("Звершение работы функции")
         return "Это не номер карты!"
 
 
 def get_mask_account(account_number: Union[str]) -> Union[str]:
     """Функция возвращает замаскированный номер счета"""
 
+    logger.info("Запуск работы функции")
+
     if isinstance(account_number, str):
         if len(account_number) == 20:
+
             logger.info("Применяется маска к номеру счета")
 
             last_numbers_string = account_number[-6:]
@@ -49,10 +58,16 @@ def get_mask_account(account_number: Union[str]) -> Union[str]:
             last_numbers_list[:2] = ["*", "*"]
             masked_account_number = "".join(last_numbers_list)
 
+            logger.info("Звершение работы функции")
+
             return masked_account_number
         else:
             logger.error(f"Произошла ошибка: Введен не номер счета")
+            logger.info("Звершение работы функции")
+
             return "Это не номер счета"
     else:
         logger.error(f"Произошла ошибка: Введен не номер счета")
+        logger.info("Звершение работы функции")
+
         return "Это не номер счета"
